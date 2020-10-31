3961d0b7149d3c043eadf4d0d15612d5a2dcf352
someonedeng <someonedeng@qq.com>
2020-11-01 02:24:16 +0800

    feat: report

--- App.go
+++ App.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func ipInfo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(strings.Split(r.RemoteAddr, ":")[0]))
+type Info struct {
+	Ip          string `json:"ip"`
+	UpdatedTime int64  `json:"updated_time"`
+}
+
+func (info *Info) ipInfo(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(info)
+	w.Write(data)
+}
+
+func (info *Info) report(w http.ResponseWriter, r *http.Request) {
+	info.Ip = strings.Split(r.RemoteAddr, ":")[0]
+	info.UpdatedTime = time.Now().Unix()
 }
 
 func main() {
-	http.HandleFunc("/ipinfo", ipInfo)
+
+	var info Info
+
+	http.HandleFunc("/ipinfo", info.ipInfo)
+	http.HandleFunc("/reportip", info.report)
 	http.ListenAndServe(":8765", nil)
 }
